3617e5bfc24a247510260e82441e3c0c4edbbd65
sunagimo-syakunage <cfgy7u6@gmail.com>
2019-02-04 12:43:40 +0900

    ステージ探索ゲージとボス戦ボタン

--- sprites/data.rb
+++ sprites/data.rb
@@ -18,10 +18,18 @@ class Game_data
     # bossはなまえの後にセリフが入れれる
     # でもメソッド側で条件分岐とかさせてセリフを変えたりもありかも
     # とりあえず敵のリスト ステージでエンカウントが違うから何個か作る
-    @spi_fire = Enemy.new('ファイアスピリット', 10, 50, 'fire', 50, 'fire_breath', 50)
-    @spi_ice = Enemy.new('アイススピリット', 25, 25, 'ice', 50, 'blizzard', 50)
-    @boss_bear = Enemy.new('熊', 1000, 300, 'nomal', 100_000)
+    @spi_fire = Enemy.new('ファイアスピリット', 1, 50, 'fire', 50, 'fire_breath', 50)
+    @spi_ice = Enemy.new('アイススピリット', 1, 25, 'ice', 50, 'blizzard', 50)
+    @boss_bear = Enemy.new('熊', 1000, 300, 'nomal', 10_000)
+    @boss_trent = Enemy.new('トレント', 2000, 500, 'nomal', 100_000)
     # ==================エネミーおわり==================
+    # ==================ステージはじめ==================
+    # 一回の探索で1exp入るので何回で100％になるか
+    @forest_stage = Stage.new('forest', 1)
+    @forest_boss_stage = Stage.new('forest_boss', 1)
+
+    @marine_stage = Stage.new('marine', 15)
+    # ==================ステージおわり==================
 
     # ==================ハッシュはじめ==================
     @@data_book = {
@@ -34,11 +42,13 @@ class Game_data
       spi_fire: @spi_fire,
       spi_ice: @spi_ice,
       boss_bear: @boss_bear,
+      boss_trent: @boss_trent,
       # enemylist
       # ("#{stage}_enemy_list").to_symみたいにするかな
       forest_enemy_list: [{ enemy: @spi_fire, encount: 0..3 },
                           { enemy: @spi_ice, encount: 4..7 },
                           { enemy: @boss_bear, encount: 8..9 }],
+      forest_boss_enemy_list: [{ enemy: @boss_trent, encount: 0..9 }],
       # forest_enemy_list: [@spi_fire, @spi_ice, @spi_fire, @spi_ice, @spi_fire, @spi_ice, @spi_fire, @spi_ice, @boss_bear],
       marine_enemy_list: [{ enemy: @boss_bear, encount: 0..9 }],
       # enemy_lists: [{ name: 'forest', list: @@background_forest },
@@ -50,7 +60,11 @@ class Game_data
       # ==================キャラクター終わり==================
 
       # ==================ステージ関連==================
-      stagelist: %w[forest marine]
+      forest_stage: @forest_stage,
+      forest_boss_stage: @forest_boss_stage,
+      marine_stage: @marine_stage,
+      # 今はやっつけで通常ステージとボスステージが交互になることによってボス戦を再現しています。
+      stagelist: [@forest_stage, @forest_boss_stage, @marine_stage]
       # ==================ステージ関連おわり==================
 
     }
--- sprites/stage.rb
+++ sprites/stage.rb
@@ -1,16 +1,24 @@
 class Stage
-  def initialize
-    @@stage_list = Data[:stagelist]
-    @@select = false
+  attr_accessor :name,:stage_exp,:stage_exp_max
+  # stageとか適当すぎるから例えば
+  # エネミーみたいにステージのインスタンスの一つが森とか
+  # 探索率もそれで管理する
+  # dataに渡す
+  # リクワイヤとかの関係でここでエネミーリストを宣言するのはやめよう
+  def initialize(name,stage_exp_max)
+    @name = name
+    @stage_exp_max = stage_exp_max
+    @stage_exp = 0
+    # @@select = false
   end
 
-  def self.run
-    Stage_UI.view
-    @@select = false
-    @@select = Stage_UI.stage_read if Input.mousePush?(M_LBUTTON)
-  end
+  # def self.run
+  #   Stage_UI.view
+  #   @@select = false
+  #   @@select = Stage_UI.stage_read if Input.mousePush?(M_LBUTTON)
+  # end
 
-  def self.select
-    @@select
-  end
+  # def self.select
+  #   @@select
+  # end
 end
